8bb0507c5f0e7848baba881e7dec5026200410a5
nms377 <nms377@nyu.edu>
2016-11-12 07:28:33 -1000

    Didn't fork again...even though I thought I did. -_- Also, my code from Thursday isn't passing.

--- calculator.js
+++ calculator.js
@@ -6,6 +6,11 @@
  * @return {object} `calculator` object that can be used
  */
 
+ function calculatorModule (){
+  var _memory = 0;
+  var _total = 0;
+  var calculator = {};
+
 
   /**
    * sets the `total` to the number passed in
@@ -13,53 +18,100 @@
    * @return { Number }    current total
    */
 
+calculator.load = function(x){
+  _total = x;
+  return _total;
+};
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+calculator.getTotal = function(){
+  return _total;
+};
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+calculator.add = function(x){
+  _total += x;
+};
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
+calculator.subtract = function(x){
+  _total -= x;
+};
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
+calculator.multiply = function(x){
+  _total *= x;
+};
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+calculator.divide = function(x){
+  _total /= x;
+};
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+calculator.recallMemory = function(){
+  return _memory;
+};
 
   /**
    * Stores the value of `total` to `memory`
    */
 
+calculator.saveMemory = function(){
+  _memory = _total;
+};
 
   /**
    * Clear the value stored at `memory`
    */
 
+calculator.clearMemory = function(){
+  _memory = 0;
+};
+
   /**
    * Validation
    */
 
+   function validation(x){
+    if (typeof x !== 'number'){
+      throw error;
+    }
+   }
+
+return {
+  load: load,
+  getTotal: getTotal,
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+  recallMemory: recallMemory,
+  saveMemory: saveMemory,
+  clearMemory: clearMemory
+};
+}
